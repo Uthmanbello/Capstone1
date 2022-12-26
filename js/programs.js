6e20e9094a9ea0c67c9fa44268ff39ffbfb6c198
Uthmanbello <uthmanbello88@gmail.com>
2022-12-26 17:52:40 +0100

    Add dynamic speaker section

--- js/programs.js
+++ js/programs.js
@@ -1,3 +1,6 @@
+    // =======================================
+    //             Programs
+    // ========================================
 const programArr = [
     {
         id: 0,
@@ -51,3 +54,44 @@ programArr.forEach((program) => {
 });
 
 programsSection.innerHTML = html;
+
+    // =======================================
+    //             Speakers
+    // ========================================
+const speakerArr = [
+    {
+        id: 0,
+        img: 'img/4375173_logo_python_icon.svg',
+        name: 'Yochai Benkler',
+        description: 'Berkman Professor of Entrepreneurial Legal Studies at Havard Law School',
+        message:'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+    },
+    {
+        id: 1,
+        img: 'img/4375173_logo_python_icon.svg',
+        name: 'SohYeong Noh',
+        description: 'Director of Art Center Nabi and board member of CC Korea',
+        message:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science, technology, humanities, and the arts.',
+    }
+]
+
+const speakersSection = document.querySelector('.speaker-sec');
+
+let html1 = '';
+
+speakerArr.forEach((speaker) => {
+    html1 += `
+    <section class="speakers">
+        <div class="speaker-img">
+            <img src="${speaker.img}">
+        </div>
+        <div class="speaker-info">
+            <h5 class="speaker-name">${speaker.name}</h5>
+            <p class="speaker-desc">${speaker.description}</p>
+            <hr class="rule2">
+            <p class="speaker-msg">${speaker.message}</p>
+        </div>
+    </section>`;
+});
+
+speakersSection.innerHTML = html1;
