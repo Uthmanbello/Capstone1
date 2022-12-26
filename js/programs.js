f53523646f470a422dae7173a140573deca010b3
Uthmanbello <uthmanbello88@gmail.com>
2022-12-26 21:09:06 +0100

    Update all the sections in the home page of mobile view

--- js/programs.js
+++ js/programs.js
@@ -61,17 +61,17 @@ programsSection.innerHTML = html;
 const speakerArr = [
     {
         id: 0,
-        img: 'img/4375173_logo_python_icon.svg',
-        name: 'Yochai Benkler',
-        description: 'Berkman Professor of Entrepreneurial Legal Studies at Havard Law School',
-        message:'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+        img: 'img/me.jpg',
+        name: 'Uthman Bello',
+        description: 'Web Developer and Software Engineer',
+        message:'Uthman is a software developer who is also into Data Science. He has a wealth of experience in tutoring.',
     },
     {
         id: 1,
-        img: 'img/4375173_logo_python_icon.svg',
-        name: 'SohYeong Noh',
-        description: 'Director of Art Center Nabi and board member of CC Korea',
-        message:'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science, technology, humanities, and the arts.',
+        img: 'img/ali.jpeg',
+        name: 'Ali Imoukhuede',
+        description: 'Web Developer and Network Engineer',
+        message:'Ali is a network engineer with a CCNP title. He is also a computer engineer who delves into software engineering components.',
     }
 ]
 
@@ -83,7 +83,7 @@ speakerArr.forEach((speaker) => {
     html1 += `
     <section class="speakers">
         <div class="speaker-img">
-            <img src="${speaker.img}">
+            <img src="${speaker.img}" class="dp">
         </div>
         <div class="speaker-info">
             <h5 class="speaker-name">${speaker.name}</h5>
