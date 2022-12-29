1e7e86afb6210ead669faa5017817971be8e8bf9
Uthmanbello <uthmanbello88@gmail.com>
2022-12-29 21:07:45 +0100

    Add toggle feature to show more speakers on homepage in mobile view

--- js/extra.js
+++ js/extra.js
@@ -1,7 +1,11 @@
 const more = document.getElementById('more');
 const moreSpeakers = document.querySelector('.add-speaker-sec');
+const showLess = document.querySelector('.less-info');
+const showMore = document.querySelector('.more-info');
 // const moreButton = document.querySelector('.sec3-link');
 
 more.addEventListener("click", () => {
-    moreSpeakers.classList.toggle('open');  
+    moreSpeakers.classList.toggle('open'); 
+    showLess.classList.toggle('open');
+    showMore.classList.toggle('open'); 
 });
