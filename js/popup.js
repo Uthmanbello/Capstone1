467e2ed18fdeace9c2a06c1e7dcba3befada7423
Uthmanbello <uthmanbello88@gmail.com>
2023-01-03 23:29:53 +0100

    Fix errors in javascript files using inters

--- js/popup.js
+++ js/popup.js
@@ -2,10 +2,10 @@ const popup = document.querySelector('.popup');
 const join = document.querySelector('.join-btn');
 
 join.addEventListener('click', () => {
-    popup.classList.remove('mob-none');
+  popup.classList.remove('mob-none');
 });
 
 const btnClosePopup = document.querySelector('.popup-close');
 btnClosePopup.addEventListener('click', () => {
-    popup.classList.add('mob-none');
-})
+  popup.classList.add('mob-none');
+});
--- js/programs.js
+++ js/programs.js
@@ -1,45 +1,45 @@
-    // =======================================
-    //             Programs
-    // ========================================
+// =======================================
+//             Programs
+// ========================================
 const programArr = [
-    {
-        id: 0,
-        icon: '<i class="fa-brands fa-python fa-3x ash"></i>',
-        title: 'Python',
-        description: 'Python is a high-level, general-purpose programming language. It is often described as a "batteries included" language.',
-    },
-    {
-        id: 1,
-        icon: '<i class="fa-brands fa-html5 fa-3x ash"></i>',
-        title: 'HTML 5',
-        description: 'HTML5 is a markup language used for structuring and presenting content on the World Wide Web.',
-    },
-    {
-        id: 2,
-        icon: '<i class="fa-brands fa-css3-alt fa-3x ash"></i>',
-        title: 'CSS 3',
-        description: 'Cascading Style Sheet level 3 is the advanced version of CSS. It is used for structuring, styling, and formatting web pages.',
-    },
-    {
-        id: 3,
-        icon: '<i class="fa-brands fa-square-js fa-3x ash"></i>',
-        title: 'JavaScript',
-        description: 'It is an object-oriented computer programming language commonly used to create interactive effects within web browsers.',
-    },
-    {
-        id: 4,
-        icon: '<i class="fa-brands fa-java fa-3x ash"></i>',
-        title: 'Java',
-        description: 'Java is a general-purpose, class-based, object-oriented programming language designed for having lesser implementation dependencies.',
-    }
-]
+  {
+    id: 0,
+    icon: '<i class="fa-brands fa-python fa-3x ash"></i>',
+    title: 'Python',
+    description: 'Python is a high-level, general-purpose programming language. It is often described as a "batteries included" language.',
+  },
+  {
+    id: 1,
+    icon: '<i class="fa-brands fa-html5 fa-3x ash"></i>',
+    title: 'HTML 5',
+    description: 'HTML5 is a markup language used for structuring and presenting content on the World Wide Web.',
+  },
+  {
+    id: 2,
+    icon: '<i class="fa-brands fa-css3-alt fa-3x ash"></i>',
+    title: 'CSS 3',
+    description: 'Cascading Style Sheet level 3 is the advanced version of CSS. It is used for structuring, styling, and formatting web pages.',
+  },
+  {
+    id: 3,
+    icon: '<i class="fa-brands fa-square-js fa-3x ash"></i>',
+    title: 'JavaScript',
+    description: 'It is an object-oriented computer programming language commonly used to create interactive effects within web browsers.',
+  },
+  {
+    id: 4,
+    icon: '<i class="fa-brands fa-java fa-3x ash"></i>',
+    title: 'Java',
+    description: 'Java is a general-purpose, class-based, object-oriented programming language designed for having lesser implementation dependencies.',
+  },
+];
 
 const programsSection = document.querySelector('.progs');
 
 let html = '';
 
 programArr.forEach((program) => {
-    html += `
+  html += `
         <section class="main-prog">
             <div class="icon">
                 ${program.icon}
@@ -55,32 +55,32 @@ programArr.forEach((program) => {
 
 programsSection.innerHTML = html;
 
-    // =======================================
-    //             Speakers
-    // ========================================
+// =======================================
+//             Speakers
+// ========================================
 const speakerArr = [
-    {
-        id: 0,
-        img: 'img/me.jpg',
-        name: 'Uthman Bello',
-        description: 'Web Developer and Software Engineer',
-        message:'Uthman is a software developer who is also into Data Science. He has a wealth of experience in tutoring.',
-    },
-    {
-        id: 1,
-        img: 'img/ali.jpeg',
-        name: 'Imoukhuede Ali',
-        description: 'Web Developer and Network Engineer',
-        message:'Imoukhuede is a network engineer with a CCNP title. He is also a computer engineer who delves into software engineering components.',
-    }
-]
+  {
+    id: 0,
+    img: 'img/me.jpg',
+    name: 'Uthman Bello',
+    description: 'Web Developer and Software Engineer',
+    message: 'Uthman is a software developer who is also into Data Science. He has a wealth of experience in tutoring.',
+  },
+  {
+    id: 1,
+    img: 'img/ali.jpeg',
+    name: 'Imoukhuede Ali',
+    description: 'Web Developer and Network Engineer',
+    message: 'Imoukhuede is a network engineer with a CCNP title. He is also a computer engineer who delves into software engineering components.',
+  },
+];
 
 const speakersSection = document.querySelector('.speaker-sec');
 
 let html1 = '';
 
 speakerArr.forEach((speaker) => {
-    html1 += `
+  html1 += `
     <section class="speakers">
         <div class="speaker-wrap">
             <div class="speaker-back"></div>
@@ -100,28 +100,28 @@ speakerArr.forEach((speaker) => {
 speakersSection.innerHTML = html1;
 
 const addSpeakerArr = [
-    {
-        id: 0,
-        img: 'img/sulaiman.jpeg',
-        name: 'Sulaiman Adoga',
-        description: 'Programmer and Audio-Visual Engineer',
-        message:'Sulaiman is a programmer and a seasoned AV Engineer with a wealth of experience in real time media.',
-    },
-    {
-        id: 1,
-        img: 'img/serg.jpeg',
-        name: 'Ataneh Muhammad',
-        description: 'Mathematical Scientist and Statistician',
-        message:'Ataneh is a mathematical consultant who specializes in AI and ML tools and components used for Data Science and Data Analytics.',
-    }
-]
+  {
+    id: 0,
+    img: 'img/sulaiman.jpeg',
+    name: 'Sulaiman Adoga',
+    description: 'Programmer and Audio-Visual Engineer',
+    message: 'Sulaiman is a programmer and a seasoned AV Engineer with a wealth of experience in real time media.',
+  },
+  {
+    id: 1,
+    img: 'img/serg.jpeg',
+    name: 'Ataneh Muhammad',
+    description: 'Mathematical Scientist and Statistician',
+    message: 'Ataneh is a mathematical consultant who specializes in AI and ML tools and components used for Data Science and Data Analytics.',
+  },
+];
 
 const addSpeakerSection = document.querySelector('.add-speaker-sec');
 
 let html2 = '';
 
 addSpeakerArr.forEach((speaker) => {
-    html2 += `
+  html2 += `
     <section class="speakers">
         <div class = "speaker-wrap">
             <div class="speaker-back"></div>
@@ -135,7 +135,7 @@ addSpeakerArr.forEach((speaker) => {
             <hr class="rule2">
             <p class="speaker-msg">${speaker.message}</p>
         </div>
-    </section>`
+    </section>`;
 });
 
 addSpeakerSection.innerHTML = html2;
